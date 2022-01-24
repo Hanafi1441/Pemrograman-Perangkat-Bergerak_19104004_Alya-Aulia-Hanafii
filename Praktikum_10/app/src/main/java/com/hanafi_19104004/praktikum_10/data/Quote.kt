21d3afe40684937bcaaceb23f0bfa0efe5f29f83
AlyaAuliaHanafi <72422058+Hanafi1441@users.noreply.github.com>
2022-01-24 22:05:07 +0700

    Add files via upload

---
+++ Praktikum_10/app/src/main/java/com/hanafi_19104004/praktikum_10/data/Quote.kt
@@ -0,0 +1,13 @@
+package com.hanafi_19104004.praktikum_10.data
+
+
+import android.os.Parcelable
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
